3ef47fd5cceb79b5bad9618a32a8aca4a610b16f
Jose Estrada <j.magallon@oohmd.com>
2019-03-22 18:24:42 -0600

    plugins databases properties

--- oh-listings-cochran.php
+++ oh-listings-cochran.php
@@ -9,18 +9,24 @@ Version: 0.1
 Author URI: http://oh.marketing
 */
 
-require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+defined('ABSPATH') or die("Sorry just aliens can access to this file");
+
+
 function oh_listings_cochran() {
     // aqui crearemos la tabla
     global $wpdb;
     $nombreTabla = $wpdb->prefix . "property";
     $charset_collate = $wpdb->get_charset_collate();
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
     $created = dbDelta(  
     "CREATE TABLE $nombreTabla (
       ID bigint(20) unsigned NOT NULL AUTO_INCREMENT,
       listing_mls varchar(150),
       listing_type varchar(10),
       listing_name varchar(130),
+      listing_headline varchar(250),
+      listing_subheadline varchar(250),
+      listing_preconstruction varchar(10),
       listing_image varchar(250),
       listing_link varchar(100),
       listing_preview text,
@@ -30,23 +36,18 @@ function oh_listings_cochran() {
       listing_bedrooms varchar(10),
       listing_bathrooms varchar(10),
       listing_address varchar(250),
-      listing_price varchar(50),
+      listing_price decimal(18,2),
       listing_long varchar(150),
       listing_lat varchar(150),
       listing_beachfront varchar(100),
       listing_petfriendly varchar(10),
+      listing_database_date date,
       PRIMARY KEY (ID)
     ) $charset_collate;"
   );
 }
 
-/*
-require_once plugin_dir_path(__FILE__).'includes/posttypes.php';
 
-register_activation_hook(__FILE__, 'mls_rewrite_flush');
- */
-
-register_activation_hook( __FILE__, 'oh_listings_cochran' ); 
 
 function oh_listings_cochran_install_data() {
     global $wpdb;
@@ -128,13 +129,18 @@ function oh_listings_cochran_install_data() {
             $listing_petfriendly = $post['petfriendly'];
             $listing_type= $post['type'];
             $listing_link= $post['link'];
-
+            $listing_headline= $post['headline'];
+            $listing_subheadline= $post['subheadline'];
+            $listing_preconstruction = $post['preconstruction'];
             $wpdb->insert( 
                 $table_name, 
                 array( 
                     'listing_mls' => $listing_mls, 
                     'listing_type'=> $listing_type,
                     'listing_name' => $listing_name, 
+                    'listing_headline'=>$listing_headline,
+                    'listing_subheadline'=> $listing_subheadline,
+                    'listing_preconstruction'=> $listing_preconstruction,
                     'listing_image' => $listing_image,      
                     'listing_link' => $listing_link,
                     'listing_preview' => $listing_preview,  
@@ -148,18 +154,19 @@ function oh_listings_cochran_install_data() {
                     'listing_long' => $listing_long,
                     'listing_lat' => $listing_lat,
                     'listing_beachfront' => $listing_beachfront,
-                    'listing_petfriendly' => $listing_petfriendly
+                    'listing_petfriendly' => $listing_petfriendly,
+                    'listing_database_date' => current_time( 'mysql')
                 ) 
             );
         }
     
     }
-    $wpdb->print_error();
-    exit();
+   // $wpdb->print_error();
+    //die;
 
 
 }
-
+register_activation_hook( __FILE__, 'oh_listings_cochran' ); 
 register_activation_hook( __FILE__, 'oh_listings_cochran_install_data' ); 
 
 
---
+++ uninstall.php
@@ -0,0 +1,13 @@
+<?php
+
+
+
+
+
+if(! defined('WP_UNINSTALL_PLUGIN')){
+    die;
+}
+global $wpdb;
+$table_name_prepared = $wpdb->prefix . "property";
+$the_removal_query = "DROP TABLE IF EXISTS {$table_name_prepared}";
+$wpdb->query( $the_removal_query );
