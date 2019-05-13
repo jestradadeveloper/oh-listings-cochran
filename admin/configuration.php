e4c1365441790a941b3d6e62c8017d9db57d1a7c
jomaestradama <jomaestradama@ittepic.edu.mx>
2019-05-13 04:42:55 -0500

    new url

--- admin/configuration.php
+++ admin/configuration.php
@@ -6,40 +6,5 @@ if (! current_user_can ('manage_options')) wp_die (__ ('No tienes suficientes pe
     Welcome to the configuration of the RETS database installer / updater
  </div>
 
-
- <form method='post'>
-        <input type='hidden' name='action' value='salvaropciones'>
-        <table>
-            <tr>
-                <td>
-                    <label for='telefono'>Telefono</label>
-                </td>
-                <td>
-                    <input type='text' name='telefono' id='telefono' >
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label for='direccion'>Dirección</label>
-                </td>
-                <td>
-                    <input type='text' name='direccion' id='direccion' >
-                </td>
-            </tr>
-            <tr>
-                <td>
-                    <label for='email'>Email</label>
-                </td>
-                <td>
-                    <input type='text' name='email' id='email' >
-                </td>
-            </tr>
-            <tr>
-                <td colspan='2'>
-                    <input type='submit' value='Enviar'>
-                </td>
-            </tr>
-        </table>
-    </form>
 <?php
  ?>
